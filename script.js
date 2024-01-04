f33d597a31aa12b10e559cb2f3eeaa19e3580be3
jewel hassan <jewelhassan399@gmail.com>
2024-01-04 21:31:13 +0600

    added new code

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-function register(){
+function saveToFile() {
     var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+    var demoContent = document.getElementById("demo")
 
-    var userData ={
-        username:username,
-        email:email,
-        password:password,
+    var userData = {
+        username: username,
+        email: email,
+        password: password,
     }
 
     var jsonData = JSON.stringify(userData);
-    var blob = new Blob([jsonData],{type:"application/json"});
+    var blob = new Blob([jsonData], { type: "application/json" });
 
-    var a = document.createElement("a");
-    a.href = window.URL.createObjectURL(blob);
-    a.download = "user_data.json";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+    var file = new File([blob], "user_data.json", { type: "application/json" });
 
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        // event.target.result contains the text from the file
+        demoContent.innerText=event.target.result;
+    };
+
+    // Read the file content
+    reader.readAsText(file);
 }
+
+;[]
